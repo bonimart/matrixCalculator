b3492eccd1a862d38451c4c918b0c3139cb9d9ad
Martin Benes <benesm44@fit.cvut.cz>
2022-05-06 11:13:08 +0200

    sem: add DenseMatrix class

---
+++ sem/src/DenseMatrix.h
@@ -0,0 +1,18 @@
+#include "Matrix.h"
+#include <vector>
+
+class DenseMatrix : public Matrix
+{
+protected:
+    std::vector<std::vector<double>> m_data;
+
+public:
+    DenseMatrix(const std::vector<std::vector<double>> &data);
+
+    virtual Matrix &operator=(const Matrix &other);
+    virtual Matrix &transpose() const override;
+    virtual Matrix &crop(const Point &shape) const override;
+
+    virtual void print(std::ostream &os) const override;
+    virtual double &get(const Point &coords) const override;
+};
