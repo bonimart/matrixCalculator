2266a11f4ca57643306da00b3ebedc1eee749abb
Martin Benes <benesm44@fit.cvut.cz>
2022-05-06 11:16:42 +0200

    sem: add SparseMatrix class

---
+++ sem/src/SparseMatrix.h
@@ -0,0 +1,18 @@
+#include "Matrix.h"
+#include <unordered_map>
+
+class SparseMatrix : public Matrix
+{
+protected:
+    std::unordered_map<Point, double> m_data;
+
+public:
+    SparseMatrix(const std::unordered_map<Point, double> &data);
+
+    virtual Matrix &operator=(const Matrix &other);
+    virtual Matrix &transpose() const override;
+    virtual Matrix &crop(const Point &shape) const override;
+
+    virtual void print(std::ostream &os) const override;
+    virtual double &get(const Point &coords) const override;
+};
